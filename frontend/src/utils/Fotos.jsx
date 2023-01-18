8f88881de480b1460ad4cc1b64bf7bc27e00cad3
Mariana Cousseiro de Lima <limatainer@MBP-de-Mariana.lan>
2023-01-18 19:19:52 +0000

    mvp concept

---
+++ frontend/src/utils/Fotos.jsx
@@ -0,0 +1,16 @@
+import ChicagoBulls from '../assets/img/ChicagoBulls.png';
+import Lakers01 from '../assets/img/Lakers01.png';
+import Lakers02 from '../assets/img/Lakers02.png';
+import TSquad from '../assets/img/TSquad.png';
+import Warriors01 from '../assets/img/Warriors01.png';
+import Warriors02 from '../assets/img/Warrios02.png';
+
+const imageArray = [ChicagoBulls,
+  Lakers01,
+  Lakers02,
+  TSquad,
+  Warriors01,
+  Warriors02
+];
+
+export default imageArray
